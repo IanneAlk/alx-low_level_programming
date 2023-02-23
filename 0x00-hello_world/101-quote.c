405594dceef98995e646da727ed6671709063dfb
IanneAlk <alukuianijait@gmail.com>
2023-02-23 07:49:37 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*the program should print exactly 'and that piece*/
+/*  of art is useful\"-Dora Korpar,2015-10-19*/
+
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19/n");
+	return (0);
+}
