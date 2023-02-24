7d1470e82225b01a4e7dfc94805b964c2456884e
IanneAlk <alukuianijait@gmail.com>
2023-02-24 08:40:58 -0800

    change6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,7 @@
-ne - a function that draws a straight line in the terminal
+#include "main.h"
+
+/**
+ * print_line - a function that draws a straight line in the terminal
  * @n: input number of times to print '_'
  * Return: a straight line
  */
